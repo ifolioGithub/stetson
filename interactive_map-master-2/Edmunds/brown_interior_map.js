4024051e09c4bc37599e30964dd008e11bfb82c4
ifolioGithub <122293662+ifolioGithub@users.noreply.github.com>
2023-01-10 09:23:42 -0500

    Add files via upload

---
+++ interactive_map-master-2/Edmunds/brown_interior_map.js
@@ -0,0 +1,29 @@
+; (function ($, window, document, undefined) {
+    $(document).ready(function () {
+        // Code from the textarea goes here...
+        $('#image-map-pro-container').imageMapPro({
+            "id": 6960,
+            "editor": {
+                "selected_shape": "poly-3664",
+                "tool": "select", "shapeCounter": {
+                    "rects": 5,
+                    "polys": 7,
+                    "paths": 5
+                }
+            }, "general": {
+                "name": "Edmunds",
+                "width": 1366, 
+                "height": 854, 
+                "naturalWidth": 1366,
+                "naturalHeight": 854
+            }, "image": {
+                "url": "https://api.ifolio.cloud/medias/F3/tO/F3tOYlTtN6mAMUCJ9avaYwx9rIkJB7qnxecuVtpH.jpg?width=1366"
+            }, "spots": [{
+                "id": "poly-5718", "title": "Ticketing", "type": "poly", "x": 20.903, "y": 61.337, "width": 17.954, "height": 11.184, "x_image_background": 20.903, "y_image_background": 61.337, "default_style": { "background_color": "#E72E37" }, "tooltip_content": { "squares_settings": { "containers": [{ "id": "sq-container-403761", "settings": { "elements": [{ "settings": { "name": "Heading", "iconClass": "fa fa-header" }, "options": { "heading": { "text": "Poly 1" } } }] } }] } }, "points": [{ "x": 0, "y": 17.66680182113561 }, { "x": 22.942790269685204, "y": 1.2284456567520297 }, { "x": 25.609083630242573, "y": 20.40652784853288 }, { "x": 49.0817864745961, "y": 3.9681716841493566 }, { "x": 49.853202778626624, "y": 9.583888244850474 }, { "x": 62.843903100979425, "y": 1.3013465312316257 }, { "x": 86.1290603441359, "y": 0 }, { "x": 100, "y": 28.76712328767127 }, { "x": 8.238399199668132, "y": 100 }]
+            }, {
+                "id": "poly-5251", "title": "HallOfFame", "type": "poly", "x": 25.958, "y": 31.768, "width": 54.885, "height": 16.853, "tooltip_content": { "squares_settings": { "containers": [{ "id": "sq-container-403761", "settings": { "elements": [{ "settings": { "name": "Heading", "iconClass": "fa fa-header" }, "options": { "heading": { "text": "Poly 2" } } }] } }] } }, "points": [{ "x": 0, "y": 0.9081852256089522 }, { "x": 0.17451938756819044, "y": 100 }, { "x": 9.59856631625035, "y": 100 }, { "x": 9.59856631625035, "y": 43.63636363636362 }, { "x": 99.99960907657182, "y": 41.8181818181818 }, { "x": 100, "y": 0 }]
+            }, { "id": "poly-3664", "title": "Lobby", "type": "poly", "x": 36.468, "y": 61.34, "width": 42.669, "height": 19.615, "tooltip_content": { "squares_settings": { "containers": [{ "id": "sq-container-403761", "settings": { "elements": [{ "settings": { "name": "Heading", "iconClass": "fa fa-header" }, "options": { "heading": { "text": "Poly 3" } } }] } }] } }, "points": [{ "x": 0, "y": 0 }, { "x": 6.734475350697804, "y": 21.87089920639881 }, { "x": 98.4286224181705, "y": 100 }, { "x": 100, "y": 79.69130787977258 }, { "x": 90.796217020713, "y": 4.705367743548088 }] }, { "id": "poly-7997", "title": "Lounge", "type": "poly", "x": 42.163, "y": 38.83, "width": 24.808, "height": 23.288, "tooltip_content": { "squares_settings": { "containers": [{ "id": "sq-container-403761", "settings": { "elements": [{ "settings": { "name": "Heading", "iconClass": "fa fa-header" }, "options": { "heading": { "text": "Poly 4" } } }] } }] } }, "points": [{ "x": 0.3861025924009192, "y": 97.36842492080562 }, { "x": 100, "y": 100 }, { "x": 100, "y": 0 }, { "x": 0, "y": 0.0018192930027857682 }] },
+            { "id": "poly-1709", "title": "Cafe", "type": "poly", "x": 66.927, "y": 54.208, "width": 8.812, "height": 8.12, "tooltip_content": { "squares_settings": { "containers": [{ "id": "sq-container-403761", "settings": { "elements": [{ "settings": { "name": "Heading", "iconClass": "fa fa-header" }, "options": { "heading": { "text": "Poly 5" } } }] } }] } }, "points": [{ "x": 76.08598956392586, "y": 0.00260886940834206 }, { "x": 100, "y": 100 }, { "x": 0, "y": 98.1150380926924 }, { "x": 0.002434881062609168, "y": 0 }] }, { "id": "poly-6316", "title": "Retail", "type": "poly", "x": 75.108, "y": 61.808, "width": 6.357, "height": 15.015, "tooltip_content": { "squares_settings": { "containers": [{ "id": "sq-container-403761", "settings": { "elements": [{ "settings": { "name": "Heading", "iconClass": "fa fa-header" }, "options": { "heading": { "text": "Poly 6" } } }] } }] } }, "points": [{ "x": 100, "y": 0.2489770607769677 }, { "x": 63.27782974291718, "y": 100 }, { "x": 0, "y": 0 }] }]
+        });
+    });
+})(jQuery, window, document);
