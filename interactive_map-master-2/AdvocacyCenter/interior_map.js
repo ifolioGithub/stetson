dd059f3e93ae2479142c084a534eb3f4ce82678b
ifolioGithub <122293662+ifolioGithub@users.noreply.github.com>
2023-01-11 12:34:58 -0500

    Update interior_map.js

--- interactive_map-master-2/AdvocacyCenter/interior_map.js
+++ interactive_map-master-2/AdvocacyCenter/interior_map.js
@@ -771,12 +771,6 @@
             "y": 100
         }]
     }]
-} // This is just a sample script. Paste your real code (javascript or HTML) here.
-
-if ('this_is' == /an_example/) {
-    of_beautifier();
-} else {
-    var a = b ? (c % d) : e[f];
 });
     });
 
