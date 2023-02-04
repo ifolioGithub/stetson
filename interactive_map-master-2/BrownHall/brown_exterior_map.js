55bb21f9a9a8079bcf58069c07577da81254d3e2
ifolioGithub <122293662+ifolioGithub@users.noreply.github.com>
2023-02-04 00:34:09 -0500

    Update brown_exterior_map.js

--- interactive_map-master-2/BrownHall/brown_exterior_map.js
+++ interactive_map-master-2/BrownHall/brown_exterior_map.js
@@ -1,229 +1,231 @@
 ;(function ($, window, document, undefined) {
     $(document).ready(function() {
         $('#image-map-pro-container').imageMapPro({
-            "id": 3226,
-            "editor": {
-                "tool": "text",
-                "currentLayer": 7,
-                "shapeCounter": {
-                    "spots": 5,
-                    "rects": 3,
-                    "ovals": 7,
-                    "polys": 45,
-                    "texts": 8,
-                    "paths": 10
-                }
-            },
-            "general": {
-                "name": "brown_hall_exterior",
-                "shortcode": "brown_hall",
-                "width": 3024,
-                "height": 1632,
-                "naturalWidth": 30240,
-                "naturalHeight": 16320
-            },
-            "image": {
-                "url": "../assets/images/Brown_Hall/floorplan/StetsonU_BrownHall_Exterior01_Interactive_large.jpg"
-            },
-            "layers": {
-                "layers_list": [{
-                    "id": 7,
-                    "title": "Exterior",
-                    "image_url": "../assets/images/Brown_Hall/floorplan/StetsonU_BrownHall_Exterior01_Interactive_large.jpg",
-                    "image_width": 3024,
-                    "image_height": 1632
-                }]
-            },
-            "spots": [{
-                "id": "poly-4408",
-                "title": "Floor 2",
-                "type": "poly",
-                "x": 0,
-                "y": 29.089,
-                "width": 99.88,
-                "height": 35.368,
-                "x_image_background": 0,
-                "y_image_background": 29.089,
-                "layerID": "7",
-                "default_style": {
-                    "background_color": "#105e31",
-                    "background_opacity": 0
-                },
-                "mouseover_style": {
-                    "background_color": "#105e31",
-                    "background_opacity": 0.6991185897435898
-                },
-                "tooltip_style": {
-                    "position": "left",
-                    "width": 250,
-                    "auto_width": 0,
-                    "offset_x": 64.01654411764706,
-                    "offset_y": 3.235932093425596
-                },
-                "tooltip_content": {
-                    "squares_settings": {
-                        "containers": [{
-                            "id": "sq-container-613701",
+    "id": 3659,
+    "editor": {
+        "previewMode": 1,
+        "tool": "select",
+        "currentLayer": 7,
+        "shapeCounter": {
+            "spots": 5,
+            "rects": 3,
+            "ovals": 7,
+            "polys": 45,
+            "texts": 9,
+            "paths": 10
+        }
+    },
+    "general": {
+        "name": "Exterior",
+        "shortcode": "brown_hall",
+        "width": 3024,
+        "height": 2160,
+        "naturalWidth": 30240,
+        "naturalHeight": 21600
+    },
+    "image": {
+        "url": "https://api.ifolio.cloud/medias/m2/N7/m2N7uGyIb1SKM41wcrGHw8xR2bK9XcqC9HHKXW6L.png"
+    },
+    "layers": {
+        "layers_list": [{
+            "id": 7,
+            "title": "Exterior",
+            "image_url": "../assets/images/Brown_Hall/floorplan/StetsonU_BrownHall_Exterior01_Interactive_large.jpg",
+            "image_width": 3024,
+            "image_height": 1632
+        }]
+    },
+    "spots": [{
+        "id": "poly-4408",
+        "title": "Floor 2",
+        "type": "poly",
+        "x": 0,
+        "y": 29.492,
+        "width": 100,
+        "height": 35.146,
+        "x_image_background": 0,
+        "y_image_background": 29.089,
+        "layerID": "7",
+        "default_style": {
+            "background_color": "#105e31",
+            "background_opacity": 0.2222222222222222
+        },
+        "mouseover_style": {
+            "background_color": "#105e31",
+            "background_opacity": 0.6991185897435898
+        },
+        "tooltip_style": {
+            "position": "left",
+            "width": 250,
+            "auto_width": 0,
+            "offset_x": 64.87977305475503,
+            "offset_y": 1.416398079676668
+        },
+        "tooltip_content": {
+            "squares_settings": {
+                "containers": [{
+                    "id": "sq-container-613701",
+                    "settings": {
+                        "elements": [{
                             "settings": {
-                                "elements": [{
-                                    "settings": {
-                                        "name": "Heading",
-                                        "iconClass": "fa fa-header"
-                                    },
-                                    "options": {
-                                        "heading": {
-                                            "text": "Main Corridor, Study Bar, Small Group Rooms, Officess, Smart Lab, Body Testing Lab, Phlebotomy Area"
-                                        }
-                                    }
-                                }]
+                                "name": "Heading",
+                                "iconClass": "fa fa-header"
+                            },
+                            "options": {
+                                "heading": {
+                                    "text": "Main Corridor, Study Bar, Small Group Rooms, Officess, Smart Lab, Body Testing Lab, Phlebotomy Area"
+                                }
                             }
                         }]
                     }
-                },
-                "points": [{
-                    "x": 0.1201923076923077,
-                    "y": 40.880503144654085
-                }, {
-                    "x": 26.802884615384613,
-                    "y": 54.088050314465406
-                }, {
-                    "x": 37.37980769230769,
-                    "y": 17.610062893081754
-                }, {
-                    "x": 51.081730769230774,
-                    "y": 27.0440251572327
-                }, {
-                    "x": 64.30288461538461,
-                    "y": 0
-                }, {
-                    "x": 99.8798076923077,
-                    "y": 40.25157232704402
-                }, {
-                    "x": 100,
-                    "y": 57.23270440251572
-                }, {
-                    "x": 97.11538461538461,
-                    "y": 57.23270440251572
-                }, {
-                    "x": 97.23557692307693,
-                    "y": 99.37106918238992
-                }, {
-                    "x": 87.86057692307693,
-                    "y": 98.11320754716984
-                }, {
-                    "x": 71.27403846153845,
-                    "y": 93.71069182389937
-                }, {
-                    "x": 59.61538461538461,
-                    "y": 93.08176100628933
-                }, {
-                    "x": 50.36052884615384,
-                    "y": 96.22674353598883
-                }, {
-                    "x": 37.5,
-                    "y": 94.96855345911953
-                }, {
-                    "x": 27.163461538461537,
-                    "y": 100
-                }, {
-                    "x": 0,
-                    "y": 99.99973584905659
                 }]
-            }, {
-                "id": "poly-7190",
-                "title": "Floor 1",
-                "type": "poly",
-                "x": 0.12,
-                "y": 62.011,
-                "width": 96.879,
-                "height": 21.132,
-                "x_image_background": 0.12,
-                "y_image_background": 62.011,
-                "width_image_background": 96.87875150060023,
-                "height_image_background": 21.131982204646565,
-                "layerID": "7",
-                "default_style": {
-                    "background_color": "#8bc540",
-                    "background_opacity": 0
-                },
-                "mouseover_style": {
-                    "background_color": "#8bc540",
-                    "background_opacity": 0.6837606837606838
-                },
-                "tooltip_style": {
-                    "position": "left",
-                    "width": 260,
-                    "auto_width": 0,
-                    "offset_x": 43.396469087635055,
-                    "offset_y": 0.11111906821551543
-                },
-                "tooltip_content": {
-                    "squares_settings": {
-                        "containers": [{
-                            "id": "sq-container-701631",
+            }
+        },
+        "points": [{
+            "x": 0.12004807692307692,
+            "y": 39.99258072089769
+        }, {
+            "x": 19.42172115384615,
+            "y": 50.4128025333635
+        }, {
+            "x": 33.29995192307692,
+            "y": 11.982529440902038
+        }, {
+            "x": 51.02043269230769,
+            "y": 26.068530087986286
+        }, {
+            "x": 70.42172115384615,
+            "y": 0
+        }, {
+            "x": 100,
+            "y": 25.010772807572618
+        }, {
+            "x": 99.88,
+            "y": 56.448276923429354
+        }, {
+            "x": 100,
+            "y": 77.11091553832546
+        }, {
+            "x": 100,
+            "y": 100
+        }, {
+            "x": 88.18714423076925,
+            "y": 97.58751742975848
+        }, {
+            "x": 71.1885096153846,
+            "y": 93.15713768292302
+        }, {
+            "x": 59.54384615384615,
+            "y": 92.52422629051797
+        }, {
+            "x": 50.300096211538445,
+            "y": 95.68911377293709
+        }, {
+            "x": 37.455,
+            "y": 94.42296046773319
+        }, {
+            "x": 27.130865384615376,
+            "y": 99.48625160697362
+        }, {
+            "x": 0,
+            "y": 99.48598578418884
+        }]
+    }, {
+        "id": "poly-7190",
+        "title": "Floor 1",
+        "type": "poly",
+        "x": 0.12,
+        "y": 62.011,
+        "width": 99.88,
+        "height": 21.132,
+        "x_image_background": 0.12,
+        "y_image_background": 62.011,
+        "width_image_background": 96.87875150060023,
+        "height_image_background": 21.131982204646565,
+        "layerID": "7",
+        "default_style": {
+            "background_color": "#8bc540",
+            "background_opacity": 0.5384615384615384,
+            "stroke_color": "#00ff00"
+        },
+        "mouseover_style": {
+            "background_color": "#8bc540",
+            "background_opacity": 0.6837606837606838
+        },
+        "tooltip_style": {
+            "position": "right",
+            "width": 260,
+            "auto_width": 0,
+            "offset_x": -46.109510086455344,
+            "offset_y": 1.9146246397694568
+        },
+        "tooltip_content": {
+            "squares_settings": {
+                "containers": [{
+                    "id": "sq-container-701631",
+                    "settings": {
+                        "elements": [{
                             "settings": {
-                                "elements": [{
-                                    "settings": {
-                                        "name": "Heading",
-                                        "iconClass": "fa fa-header"
-                                    },
-                                    "options": {
-                                        "heading": {
-                                            "text": "Teaching Kitchen Lab, Study Spaces, Classrooms, Lobby, Offices, Wet Teaching Lab, Seminar Room, Greenhouse, Environmental Science & Studies Research Lab Mud Room"
-                                        }
-                                    }
-                                }]
+                                "name": "Heading",
+                                "iconClass": "fa fa-header"
+                            },
+                            "options": {
+                                "heading": {
+                                    "text": "Teaching Kitchen Lab, Study Spaces, Classrooms, Lobby, Offices, Wet Teaching Lab, Seminar Room, Greenhouse, Environmental Science & Studies Research Lab Mud Room"
+                                }
                             }
                         }]
                     }
-                },
-                "points": [{
-                    "x": 0.000049566294919471385,
-                    "y": 88.42105263157897
-                }, {
-                    "x": 0,
-                    "y": 12.635211695906456
-                }, {
-                    "x": 27.63320941759603,
-                    "y": 11.577847953216343
-                }, {
-                    "x": 38.28996282527881,
-                    "y": 3.1573450292397665
-                }, {
-                    "x": 51.42503097893433,
-                    "y": 6.315789473684212
-                }, {
-                    "x": 60.966542750929364,
-                    "y": 0
-                }, {
-                    "x": 71.25154894671624,
-                    "y": 1.0499906432748591
-                }, {
-                    "x": 90.45848822800494,
-                    "y": 9.475775438596521
-                }, {
-                    "x": 100,
-                    "y": 12.631578947368425
-                }, {
-                    "x": 99.62825278810409,
-                    "y": 71.57894736842107
-                }, {
-                    "x": 90.33457249070632,
-                    "y": 74.73684210526315
-                }, {
-                    "x": 64.80793060718712,
-                    "y": 100
-                }, {
-                    "x": 57.49690210656754,
-                    "y": 88.42105263157897
-                }, {
-                    "x": 38.413878562577445,
-                    "y": 96.84210526315786
-                }, {
-                    "x": 28.004956629491947,
-                    "y": 78.94736842105262
                 }]
-            }]
-        });
+            }
+        },
+        "points": [{
+            "x": 0.00004807702328297374,
+            "y": 88.421052631579
+        }, {
+            "x": 0,
+            "y": 12.635211695906449
+        }, {
+            "x": 26.80294048024916,
+            "y": 11.577847953216372
+        }, {
+            "x": 37.13950048608516,
+            "y": 3.1573450292397753
+        }, {
+            "x": 49.87991165606906,
+            "y": 6.315789473684212
+        }, {
+            "x": 59.134738638038506,
+            "y": 0
+        }, {
+            "x": 69.11072096925233,
+            "y": 1.0499906432748658
+        }, {
+            "x": 87.74056749139858,
+            "y": 9.475775438596546
+        }, {
+            "x": 99.99999999999997,
+            "y": 10.724518565009921
+        }, {
+            "x": 100,
+            "y": 70.62305109736297
+        }, {
+            "x": 87.6203749331912,
+            "y": 74.73684210526315
+        }, {
+            "x": 62.860707942467755,
+            "y": 100
+        }, {
+            "x": 55.76934700823144,
+            "y": 88.421052631579
+        }, {
+            "x": 37.259693044292554,
+            "y": 96.84210526315786
+        }, {
+            "x": 25.576613869729176,
+            "y": 89.44803092341863
+        }]
+    }]
+});
     });
 })(jQuery, window, document);
